c5f94689ba1012740ea4a2c158b6b4d0f50f47ae
Micheal Potros <PotrosM@itservicemanagementcqcorg.onmicrosoft.com>
2023-12-18 16:18:18 +0100

    Signed-off-by: Micheal Potros <PotrosM@itservicemanagementcqcorg.onmicrosoft.com>

--- PCFDatePickerValidator/index.ts
+++ PCFDatePickerValidator/index.ts
@@ -5,7 +5,6 @@ export class PCFDatePickerValidator implements ComponentFramework.StandardContro
     private container: HTMLDivElement;
     private monthDropdown: HTMLSelectElement;
     private daysDropdown: HTMLSelectElement;
-    private yearsDropdown: HTMLSelectElement;
     private baseDate: string;
     private dateFormat: string;
     private currentYear: number;
@@ -20,11 +19,13 @@ export class PCFDatePickerValidator implements ComponentFramework.StandardContro
     private _notifyOutputChanged: () => void;
     private monthSelectElement: HTMLSelectElement;
 	private daySelectElement: HTMLSelectElement;
-
-    constructor() {
+    private yearSelectElement: HTMLSelectElement;
+    private divParentElement: HTMLDivElement;
+    
+	constructor() {
 
     }
-public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
+    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
 	{
 		this.context = context;
 		this.container = container;
@@ -48,8 +49,17 @@ public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: (
 
 		this._notifyOutputChanged = notifyOutputChanged;
 
-		this.daySelectValues = ["Select day", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
-		this.monthSelectValues = {"Select month": "0", "Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06", "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12" };
+		this.daySelectValues = ["Select day"];
+		this.daySelectElement = document.createElement("select");
+		this.daySelectElement.classList.add("dnl-second-select");
+		//this.container.append(this.daySelectElement);
+		this.divParentElement = document.createElement('div');
+        this.divParentElement.classList.add("container");
+        this.divParentElement.appendChild(this.daySelectElement);
+
+		this.InitDayDropdown(false,this.daySelectValues);
+
+		this.monthSelectValues = {"Select month" :"0"};
 		let yearStart =  this.currentYear - 120;
         let yearEnd =  this.currentYear +  this.futureYearsCount;
         let yearsArr = new Array<string>();
@@ -63,33 +73,50 @@ public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: (
         
         this.yearSelectValues= yearsArr.slice();
 
-		this.InitDayDropdown();
-		this.InitMonthDropdown();
-		this.InitYearsDropdown();
+		this.InitMonthDropdown(false);
+		this.InityearSelectElement();
+		this.container.append(this.divParentElement);
 	}
 
-	private InitYearsDropdown() {
+	private InityearSelectElement() {
         let context = this;
-        this.yearsDropdown = document.createElement("select");
-        this.yearsDropdown.classList.add("dnl-third");
-        this.yearsDropdown.classList.add("dnl-first-select");
+        this.yearSelectElement = document.createElement("select");
+        this.yearSelectElement.classList.add("dnl-third");
+        this.yearSelectElement.classList.add("dnl-first-select");
 
         this.yearSelectValues.forEach(function (el) {
-            context.yearsDropdown.add(new Option(el));
+            context.yearSelectElement.add(new Option(el));
         });
          
         //to fix the days count for non-leap/ leap years
-        this.yearsDropdown.addEventListener("change", this.firstChange.bind(this));
-        this.yearsDropdown.selectedIndex = 0; 
-        this.container.append(this.yearsDropdown);
-
+        this.yearSelectElement.addEventListener("change", this.onYearChange.bind(this));
+        this.yearSelectElement.selectedIndex = 0; 
+        this.divParentElement.appendChild(this.yearSelectElement);
+		//this.container.append(this.yearSelectElement);
     }
 
-	private InitMonthDropdown() {
+	private onYearChange(){
+		//reset the days dropdown
+		this.daySelectValues = ["Select day"];
+		this.clearDaysAllOptions();
+		this.InitDayDropdown(false,this.daySelectValues);
+		//initiate the month dropdown
+		this.clearMonthsAllOptions();
+		this.InitMonthDropdown(true);
+	}
+
+	private InitMonthDropdown(isFromYear: boolean) {
 		let context = this;
+		if(isFromYear){
+		this.monthSelectValues = {"Select month": "0", "Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06", "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"};
+		}
+		else{
 		this.monthSelectElement = document.createElement("select");
 		this.monthSelectElement.classList.add("dnl-first-select");
+		//this.container.append(this.monthSelectElement);
+        this.divParentElement.appendChild(this.monthSelectElement);
 
+		}
 		Object.keys(this.monthSelectValues).forEach(function (el) {
 			context.monthSelectElement.add(new Option(el));
 		});
@@ -97,60 +124,57 @@ public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: (
 		// this.monthSelectElement.selectedIndex = this.firstValue ? Object.keys(this.monthSelectValues).indexOf(monthName) : 0;
 		this.monthSelectElement.selectedIndex = 0;
 		this.monthSelectElement.addEventListener("change", this.firstChange.bind(this));
-		this.container.append(this.monthSelectElement);
 	}
 
-	private InitDayDropdown() {
-		let context = this;
-		this.daySelectElement = document.createElement("select");
-		this.daySelectElement.classList.add("dnl-second-select");
-
+	private InitDayDropdown(isFromMonth: boolean,daySelectValues: Array<string>) {
+		let daysInMonth = 0;
 		let currentMonth = this.firstValue && this.firstValue[0] == "0" && this.firstValue.length > 1 ? this.firstValue.substring(1) : this.firstValue == "---" || !this.firstValue ? "1" : this.firstValue;
 		let currentYear = new Date().getFullYear();
-		let daysInMonth = this.getDaysInMonth(parseInt(currentMonth, 10), currentYear);
+
+		if(isFromMonth){
+			daysInMonth = this.getDaysInMonth(parseInt(currentMonth, 10), currentYear);
+		}
 		for (let i = 0; i <= daysInMonth; i++) {
-			context.daySelectElement.add(new Option(this.daySelectValues[i]));
+			this.daySelectElement.add(new Option(daySelectValues[i]));
 		}
 		//this.daySelectElement.selectedIndex = this.secondValue ? this.daySelectValues.indexOf(this.secondValue) : 0;
-		this.daySelectElement.selectedIndex =0;
-		this.daySelectElement.addEventListener("change", this.secondChange.bind(this));
-		this.container.append(this.daySelectElement);
+		this.daySelectElement.selectedIndex = 0;
+		
 	}
 
 	private firstChange() {
-		this.firstValue = this.monthSelectElement[this.monthSelectElement.selectedIndex].innerText;
-		this.firstValue = this.monthSelectValues[this.firstValue];
+		this.daySelectValues = ["Select day", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+		this.InitDayDropdown(true,this.daySelectValues);
 
-		let currentMonth = this.firstValue && this.firstValue[0] == "0" && this.firstValue.length > 1 ? this.firstValue.substring(1) : this.firstValue == "---" || !this.firstValue ? "1" : this.firstValue;
-		let currentYear = new Date().getFullYear();
-		let daysInMonth = this.getDaysInMonth(parseInt(currentMonth, 10), currentYear);
+		let currentMonth = this.monthSelectElement.selectedIndex;
+		let currentYear = this.yearSelectElement[this.yearSelectElement.selectedIndex].innerText
+		let daysInMonth = this.getDaysInMonth(currentMonth, parseInt(currentYear));
 		let daysArray = this.daySelectValues.filter(el => { return el !== "---" && parseInt(el, 10) <= daysInMonth });
-		this.clearAllOptions();
+		this.clearDaysAllOptions();
 		for (let i = 0; i <= daysInMonth; i++) {
 			this.daySelectElement.add(new Option(this.daySelectValues[i]));
 		}
-		if (this.secondValue && this.secondValue !== "0") {
-			this.daySelectElement.selectedIndex = this.secondValue && daysArray.includes(this.secondValue) ? daysArray.indexOf(this.secondValue) + 1 : daysArray.length;
-			this.secondChange();
-		}
-
+	this.daySelectElement.selectedIndex= 0;
 		this._notifyOutputChanged();
 	}
-
-	private secondChange() {
-		this.secondValue = this.daySelectElement[this.daySelectElement.selectedIndex].innerText.replace("---", "0");
-		this._notifyOutputChanged();
-	}
-
+ 
 	private getDaysInMonth(month: number, year: number) {
 		return new Date(year, month, 0).getDate();
 	}
 
-	private clearAllOptions() {
+	private clearDaysAllOptions() {
 		let i = 0;
 		let selectLength = this.daySelectElement.options.length - 1;
 		for (i = selectLength; i >= 0; i--) {
 			this.daySelectElement.remove(i);
+		}
+	}
+
+	private clearMonthsAllOptions() {
+		let i = 0;
+		let selectLength = this.monthSelectElement.options.length - 1;
+		for (i = selectLength; i >= 0; i--) {
+			this.monthSelectElement.remove(i);
 		}
 	}
 
