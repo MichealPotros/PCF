d2a8a5168c60a2d87c685ef3daba18074a3b2f4c
Micheal Potros <PotrosM@itservicemanagementcqcorg.onmicrosoft.com>
2023-12-18 17:01:37 +0100

    Signed-off-by: Micheal Potros <PotrosM@itservicemanagementcqcorg.onmicrosoft.com>

--- PCFDatePickerValidator/index.ts
+++ PCFDatePickerValidator/index.ts
@@ -3,15 +3,9 @@ import { IInputs, IOutputs } from "./generated/ManifestTypes";
 export class PCFDatePickerValidator implements ComponentFramework.StandardControl<IInputs, IOutputs> {
     private context: ComponentFramework.Context<IInputs>;
     private container: HTMLDivElement;
-    private monthDropdown: HTMLSelectElement;
-    private daysDropdown: HTMLSelectElement;
     private baseDate: string;
     private dateFormat: string;
     private currentYear: number;
-    private currentMonth: number;
-    private today: number;
-    private firstValue: string;
-    private secondValue: string;
     private daySelectValues: Array<string>;
     private monthSelectValues: any;
     private yearSelectValues: Array<string>;
@@ -21,7 +15,8 @@ export class PCFDatePickerValidator implements ComponentFramework.StandardContro
 	private daySelectElement: HTMLSelectElement;
     private yearSelectElement: HTMLSelectElement;
     private divParentElement: HTMLDivElement;
-    
+    private selectedDay: string;
+
 	constructor() {
 
     }
@@ -30,35 +25,18 @@ export class PCFDatePickerValidator implements ComponentFramework.StandardContro
 		this.context = context;
 		this.container = container;
 		this.baseDate = context.parameters.Field.raw && context.parameters.Field.raw.length ? context.parameters.Field.raw : "";
-		this.dateFormat = context.parameters.DateFormat.raw && context.parameters.DateFormat.raw.length ? context.parameters.DateFormat.raw : "";
+		//this.dateFormat = context.parameters.DateFormat.raw && context.parameters.DateFormat.raw.length ? context.parameters.DateFormat.raw : "";
 		this.currentYear = new Date().getFullYear();
-        this.currentMonth = new Date().getMonth()+1;
-        this.today = new Date().getDate();
         this.futureYearsCount = 30;
-
-		switch (this.dateFormat) {
-			case "0": //YYYY-MM
-				this.firstValue = this.baseDate ? this.baseDate.split("-")[1] : "";
-				this.secondValue = this.baseDate ? this.baseDate.split("-")[0] : "";
-				break;
-			case "1":  //MM/DD
-				this.firstValue = this.baseDate ? this.baseDate.split("/")[0] : "";
-				this.secondValue = this.baseDate ? this.baseDate.split("/")[1] : "";
-				break;
-		}
-
 		this._notifyOutputChanged = notifyOutputChanged;
 
 		this.daySelectValues = ["Select day"];
 		this.daySelectElement = document.createElement("select");
-		this.daySelectElement.classList.add("dnl-second-select");
-		//this.container.append(this.daySelectElement);
+		this.daySelectElement.classList.add("dnl-second-select"); 
 		this.divParentElement = document.createElement('div');
         this.divParentElement.classList.add("container");
         this.divParentElement.appendChild(this.daySelectElement);
-
-		this.InitDayDropdown(false,this.daySelectValues);
-
+		this.InitDayDropdown();
 		this.monthSelectValues = {"Select month" :"0"};
 		let yearStart =  this.currentYear - 120;
         let yearEnd =  this.currentYear +  this.futureYearsCount;
@@ -99,7 +77,7 @@ export class PCFDatePickerValidator implements ComponentFramework.StandardContro
 		//reset the days dropdown
 		this.daySelectValues = ["Select day"];
 		this.clearDaysAllOptions();
-		this.InitDayDropdown(false,this.daySelectValues);
+		this.InitDayDropdown();
 		//initiate the month dropdown
 		this.clearMonthsAllOptions();
 		this.InitMonthDropdown(true);
@@ -120,41 +98,32 @@ export class PCFDatePickerValidator implements ComponentFramework.StandardContro
 		Object.keys(this.monthSelectValues).forEach(function (el) {
 			context.monthSelectElement.add(new Option(el));
 		});
-		// let monthName = Object.keys(this.monthSelectValues).find(el => this.monthSelectValues[el] == this.firstValue) as string;
-		// this.monthSelectElement.selectedIndex = this.firstValue ? Object.keys(this.monthSelectValues).indexOf(monthName) : 0;
 		this.monthSelectElement.selectedIndex = 0;
 		this.monthSelectElement.addEventListener("change", this.firstChange.bind(this));
 	}
 
-	private InitDayDropdown(isFromMonth: boolean,daySelectValues: Array<string>) {
-		let daysInMonth = 0;
-		let currentMonth = this.firstValue && this.firstValue[0] == "0" && this.firstValue.length > 1 ? this.firstValue.substring(1) : this.firstValue == "---" || !this.firstValue ? "1" : this.firstValue;
-		let currentYear = new Date().getFullYear();
-
-		if(isFromMonth){
-			daysInMonth = this.getDaysInMonth(parseInt(currentMonth, 10), currentYear);
-		}
-		for (let i = 0; i <= daysInMonth; i++) {
-			this.daySelectElement.add(new Option(daySelectValues[i]));
-		}
-		//this.daySelectElement.selectedIndex = this.secondValue ? this.daySelectValues.indexOf(this.secondValue) : 0;
+	private InitDayDropdown() {
+		this.daySelectValues = ["Select day"];
+		this.daySelectElement.add(new Option(this.daySelectValues[0]));
 		this.daySelectElement.selectedIndex = 0;
-		
+		this.daySelectElement.addEventListener("change", this.dayChange.bind(this));
+	}
+
+	private dayChange(){
+		this.selectedDay = this.daySelectElement[this.daySelectElement.selectedIndex].innerText;
+		this._notifyOutputChanged();
 	}
 
 	private firstChange() {
 		this.daySelectValues = ["Select day", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
-		this.InitDayDropdown(true,this.daySelectValues);
-
 		let currentMonth = this.monthSelectElement.selectedIndex;
 		let currentYear = this.yearSelectElement[this.yearSelectElement.selectedIndex].innerText
 		let daysInMonth = this.getDaysInMonth(currentMonth, parseInt(currentYear));
-		let daysArray = this.daySelectValues.filter(el => { return el !== "---" && parseInt(el, 10) <= daysInMonth });
 		this.clearDaysAllOptions();
 		for (let i = 0; i <= daysInMonth; i++) {
 			this.daySelectElement.add(new Option(this.daySelectValues[i]));
 		}
-	this.daySelectElement.selectedIndex= 0;
+		this.daySelectElement.selectedIndex= 0;
 		this._notifyOutputChanged();
 	}
  
@@ -185,17 +154,7 @@ export class PCFDatePickerValidator implements ComponentFramework.StandardContro
 
 	public getOutputs(): IOutputs
 	{
-		let format = "";
-		switch (this.dateFormat) {
-			case "0": //YYYY-MM
-				format = `${this.secondValue}-${this.firstValue}`;
-				break;
-			case "1": //MM/DD
-				format = `${this.firstValue}/${this.secondValue}`
-		}
-		return {
-			Field: (!this.secondValue || this.secondValue === "0") && (!this.firstValue || this.firstValue === "0") ? undefined : format
-		};
+		return {Field: `${this.daySelectElement[this.daySelectElement.selectedIndex].innerText}-${this.monthSelectElement[this.monthSelectElement.selectedIndex].innerText}-${this.yearSelectElement[this.yearSelectElement.selectedIndex].innerText}`};
 	}
 
 	public destroy(): void
